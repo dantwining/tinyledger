81a5b290be66ca3382efa679af372be336e9e7a0
Dan Twining <dantwining@gmail.com>
2026-09-01 20:48:30 +0100

    Commit default Spring Initializr project
    Project starter downloaded from https://start.spring.io/
    
    Options chosen:
    - Maven
    - Java 25
    - Spring Boot 4.1.1
    - Dependencies: Spring Web, HTTP Client
    
    Bookmark:
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=4.1.1&packaging=jar&configurationFileFormat=properties&jvmVersion=25&groupId=com.github.dantwining&artifactId=tinyledger&packageName=com.github.dantwining.tinyledger&dependencies=web,spring-restclient
    
    Verified with `./mvnw clean install`

---
+++ src/main/java/com/github/dantwining/tinyledger/TinyledgerApplication.java
@@ -0,0 +1,13 @@
+package com.github.dantwining.tinyledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyledgerApplication.class, args);
+	}
+
+}
